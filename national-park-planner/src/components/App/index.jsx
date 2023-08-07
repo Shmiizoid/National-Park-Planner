b3eba5816e54732712d84b5605a19e165a881b3c
Shmiizoid <eafuentes949@gmail.com>
2023-08-06 22:47:59 -0700

    console log showing data capped at 50 obj

--- national-park-planner/src/components/App/index.jsx
+++ national-park-planner/src/components/App/index.jsx
@@ -1,22 +1,28 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './styles.css'
 
 
 function App() {
+  const [parks, setParks] = useState([])
 
   useEffect(() => {
 
     async function getData() {
       const res = await fetch('https://developer.nps.gov/api/v1/parks?parkCode=&api_key=EHE50dVZ9QSYk40I3F5PyWw0xg6XWf7tgecRSSyx')
       const { data } = await res.json()
-      // console.log()
+      setParks(data)
+      console.log(data)
     }
 
     getData()
   }, [])
 
   return (
+
+    <>
     <h1 className="text-3xl font-bold underline">National Parks Planner</h1>
+    {parks.length > 0 ? <img src={parks[1].images.url} /> : <p>Parks loading...</p>}
+    </>
   )
 }
 
