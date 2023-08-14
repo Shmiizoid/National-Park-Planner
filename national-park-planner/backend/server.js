3dbd1e02b0a86202dc4c01ee40a969c044cb60ac
Shmiizoid <eafuentes949@gmail.com>
2023-08-14 09:32:43 -0700

    file structure

--- national-park-planner/backend/server.js
+++ national-park-planner/backend/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors')
 const path = require('path')
 
+// use the React build folder for static files
+app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
 
 const db = require('./models');
 
@@ -21,6 +23,11 @@ app.use(express.json())
 // /* Mount routes*/
 app.use('/api/reviews', reviewsCtrl)
 app.use('/api/users', usersCtrl)
+
+// Any other route not matching the routes above gets routed by React
+app.get('*', (req, res) => {
+    res.sendFile(path.join(path.dirname(__dirname), 'frontend', 'dist', 'index.html'));
+});
 
 
 app.listen(process.env.PORT, function () {
