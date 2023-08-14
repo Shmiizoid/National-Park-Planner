bdaf552486f5ba37b431d85c477f34ba3951b28f
Shmiizoid <eafuentes949@gmail.com>
2023-08-14 11:25:48 -0700

    sss

--- national-park-planner/backend/server.js
+++ national-park-planner/backend/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const path = require('path')
 const app = express();
 // use the React build folder for static files
-app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
+
 
 const db = require('./models');
 
@@ -18,6 +18,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
+app.use(express.static(path.join(path.dirname(__dirname), 'frontend', 'dist')))
 
 // /* Mount routes*/
 app.use('/api/reviews', reviewsCtrl)
